e10904583a7a417fbbee01308f74d9f8e0e5059f
ejqassem <eyadqassem@Eyads-MacBook-Air.local>
2017-05-21 12:35:44 -0400

    updated scripts property in package.json

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@
 var mysql = require('mysql');
 var connection; // initializing a connection variable to allow for access in orm.js file
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
